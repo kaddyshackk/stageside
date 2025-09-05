f59852a4ae0c4ea5c5bb73a3914bfd2f80124a5b
kaddyshackk <jp.breeze@proton.me>
2025-09-04 23:00:13 -0400

    Add appsettings configuration per environment

--- Api/Configuration/ServiceCollectionExtensions.cs
+++ Api/Configuration/ServiceCollectionExtensions.cs
@@ -1,9 +1,13 @@
-﻿namespace ComedyPull.Api.Startup
+using ComedyPull.Api.Configuration;
+
+namespace ComedyPull.Api.Startup
 {
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<ApiSettings>(configuration.GetSection("ApiSettings"));
+            
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
